c805d554d2b04ae65aecbe0ab5f2fb3c1a720494
Sidharth <sidharth2004@yahoo.com>
2024-10-05 01:05:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Generative AI',
+    version='0.0.0',
+    author='Bappy',
+    author_email = 'boktiar@ineuron.ai',
+    packages = find_packages(),
+    install_requires = []
+)
